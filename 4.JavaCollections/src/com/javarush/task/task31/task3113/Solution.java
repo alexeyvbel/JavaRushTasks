c67b1fdb7f62da57b729760199282a8e7f973a11
AlexJ <alexeyvbel@gmail.com>
2021-11-03 00:24:06 +0300

    4.JavaCollections - task 3113

---
+++ 4.JavaCollections/src/com/javarush/task/task31/task3113/Solution.java
@@ -0,0 +1,53 @@
+package com.javarush.task.task31.task3113;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+
+/*
+Что внутри папки?
+*/
+
+public class Solution {
+    static long totalFolders = 0;
+    static long totalFiles = 0;
+    static long totalSize = 0;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String pathEnter = reader.readLine();
+
+        Path path = Paths.get(pathEnter);
+
+        if (!Files.isDirectory(path)){
+            System.out.println(pathEnter + " - не папка");
+        }
+
+        Files.walkFileTree(path, new Visitor());
+
+        System.out.println("Всего папок - " + (totalFolders-1));
+        System.out.println("Всего файлов - " + totalFiles);
+        System.out.println("Общий размер - " + totalSize);
+    }
+
+    private static class Visitor extends SimpleFileVisitor<Path>{
+
+        @Override
+        public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+            totalFolders += 1;
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            totalFiles += 1;
+            totalSize = totalSize + attrs.size();
+            return FileVisitResult.CONTINUE;
+        }
+    }
+}
