f6e85b08924548fbf4388f5b897822f5de5e2bf2
Belyanin Alexey <alexeyvbel@gmail.com>
2022-11-21 00:17:53 +0300

    4.JavaCollections - tasks

--- 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
+++ 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
@@ -2,6 +2,8 @@ package com.javarush.task.task20.task2028;
 
 import java.io.Serializable;
 import java.util.AbstractList;
+import java.util.Collection;
+import java.util.List;
 
 /* 
 Построй дерево(1)
@@ -9,13 +11,89 @@ import java.util.AbstractList;
 
 public class CustomTree extends AbstractList<String> implements Cloneable, Serializable {
 
+    private final Entry<String> root;
+
+    public CustomTree(Entry<String> root) {
+        this.root = root;
+    }
+
     @Override
     public String get(int index) {
-        return null;
+        throw  new UnsupportedOperationException();
+    }
+
+    @Override
+    public String set(int index, String element) {
+        throw  new UnsupportedOperationException();
+    }
+
+    @Override
+    public void add(int index, String element){
+        throw  new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean add(String element){
+
+        if (root.availableToAddLeftChildren){
+            add(indexOf(root),element);
+        } else {
+
+        }
+        return true;
+    }
+
+    @Override
+    public String remove(int index) {
+        throw  new UnsupportedOperationException();
+    }
+
+    @Override
+    public List<String> subList(int fromIndex, int toIndex){
+        throw  new UnsupportedOperationException();
+    }
+
+    @Override
+    protected void removeRange(int fromIndex, int toIndex) {
+        throw  new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends String> c) {
+        throw  new UnsupportedOperationException();
     }
 
     @Override
     public int size() {
         return 0;
+    }
+
+    public String getParent(String s) {
+
+        return null;
+    }
+
+    static class Entry<T> implements Serializable{
+        String elementName;
+        boolean availableToAddLeftChildren, availableToAddRightChildren;
+        Entry<T> parent, leftChild, rightChild;
+
+        public Entry(String elementName) {
+            this.elementName = elementName;
+            availableToAddLeftChildren = true;
+            availableToAddRightChildren = true;
+        }
+
+        void checkChildren() {
+            if (leftChild != null)
+                availableToAddLeftChildren = false;
+            if (rightChild != null)
+                availableToAddRightChildren = false;
+        }
+
+        boolean isAvailableToAddChildren() {
+            return availableToAddLeftChildren || availableToAddRightChildren;
+        }
+
     }
 }
--- 4.JavaCollections/src/com/javarush/task/task20/task2028/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task20/task2028/Solution.java
@@ -1,33 +1,18 @@
 package com.javarush.task.task20.task2028;
 
-import java.util.AbstractList;
 import java.util.List;
-
-/**
- * Построй дерево(1)
- * Амиго, похоже ты уже достаточно окреп. Самое время проверить свои навыки в большой задаче!
- *
- * Сегодня реализуем свое дерево немного нестандартным способом(на базе AbstractList).
- *
- * Вводную информацию можешь получить используя свой любимый поисковик и текст ниже.
- *
- * Элементы дерева должны следовать так как показано на картинке:
- *
- *
- * Для начала сделаем наше дерево потомком класса AbstractList с параметром типа String, а также реализуем интерфейсы Cloneable и Serializable.
- *
- * Реализацию методов get(int index) и size() пока оставь стандартной.
- *
- *
- * Requirements:
- * 1. Класс CustomTree должен поддерживать интерфейс Cloneable.
- * 2. Класс CustomTree должен поддерживать интерфейс Serializable.
- * 3. Класс CustomTree должен быть потомком класса AbstractList<String>.
- *
- */
 
 public class Solution {
     public static void main(String[] args) {
-        List<String> list = new CustomTree();
+        CustomTree.Entry<String> root = null;
+        List<String> list = new CustomTree(root);
+
+        for (int i = 1; i < 16; i++) {
+            list.add(String.valueOf(i));
+        }
+
+        System.out.println("The list size is " + list.size());
+        System.out.println("The expected parent is 3. The actual parent is " + ((CustomTree) list).getParent("8"));
+        System.out.println("The expected parent is null. The actual parent is " + ((CustomTree) list).getParent("20"));
     }
 }
